1fe94ea2ce4f65e429d419c4287b6b2bc1e0a61c
Fabrizio112 <fabri.avila3@gmail.com>
2023-09-14 09:17:42 -0300

    Add Technologies icons learned

--- src/components/skills/TechCard.jsx
+++ src/components/skills/TechCard.jsx
@@ -2,10 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext } from "react";
 import ThemeContext from "../../context/ThemeContext";
 
-function TechCard({ component, text }) {
+function TechCard({ component, text, imagen }) {
     const { themeLight } = useContext(ThemeContext)
     return (<section id={themeLight ? "tech-card-light" : "tech-card-dark"}>
-        <FontAwesomeIcon size="3x" icon={component} style={themeLight ? { color: "black" } : { color: "#ffffff", }} />
+        {component && <FontAwesomeIcon size="3x" icon={component} style={themeLight ? { color: "black" } : { color: "#ffffff", }} />}
+        {imagen && <img src={imagen} />}
         <span>{text}</span>
     </section>);
 }
